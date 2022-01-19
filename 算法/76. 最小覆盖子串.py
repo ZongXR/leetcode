9952ab0156218d27a7a6c9ae957293c9827d0b08
zong.xiangrui <zxr@tju.edu.cn>
2022-01-19 11:13:04 +0800

    完善算法76题答案
    贪婪算法
    执行结果：通过
    执行用时：716 ms, 在所有 Python3 提交中击败了5.00%的用户
    内存消耗：15.4 MB, 在所有 Python3 提交中击败了9.16%的用户

--- 算法/76. 最小覆盖子串.py
+++ 算法/76. 最小覆盖子串.py
@@ -2,55 +2,100 @@
 """
 给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
 """
-from copy import deepcopy
+from collections import Counter
 
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        original_letters = self.get_dict(t)
-        global_flag = False
+        flag = False
         result = s
         i = 0
         while i < len(s):
-            letters = deepcopy(original_letters)
             if s[i] in t:
-                letters[s[i]] = letters[s[i]] - 1 if letters[s[i]] - 1 >= 0 else 0
-                j = i
-                flag = False
-                while j < len(s):
-                    if i != j and s[j] in t:
-                        letters[s[j]] = letters[s[j]] - 1 if letters[s[j]] - 1 >= 0 else 0
-                    j = j + 1
-                    if sum(letters.values()) == 0:
-                        flag = True
-                        global_flag = True
-                        break
-                result = s[i:j] if len(s[i:j]) < len(result) and flag else result
+                break
+            i = i + 1
+        j = i + len(t)
+        letters_need = self.get_dict(s[i:j], t)
+        while i < len(s) and j < len(s):
+            if len(s[i:]) < len(t):
+                # 覆盖不住了
+                break
+            if s[i] in t and j < i + len(t):
+                j = i + len(t)
+                letters_need = self.get_dict(s[i:j], t)
+            if s[i] in t and s[j - 1] in t and self.is_ok(letters_need):
+                # 如果正好碰上了
+                result = s[i:j] if len(s[i:j]) < len(result) else result
+                flag = True
                 i = i + 1
+                # 左边界，原来有的被吐出来了，要加上
+                letters_need[s[i - 1]] = letters_need[s[i - 1]] + 1
+                while i < len(s):
+                    if s[i] in t:
+                        break
+                    i = i + 1
+            else:
+                j = j + 1
+                # 右边界，原来没有的，被囊括进来的，需要减去
+                if j <= len(s) and s[j - 1] in t:
+                    letters_need[s[j - 1]] = letters_need[s[j - 1]] - 1
+        while i < len(s) and j <= len(s) and s[j - 1] in t:
+            if len(s[i:]) < len(t):
+                break
+            if s[i] in t and s[j - 1] in t and self.is_ok(letters_need):
+                result = s[i:j] if len(s[i:j]) < len(result) else result
+                flag = True
+                i = i + 1
+                letters_need[s[i - 1]] = letters_need[s[i - 1]] + 1
             else:
                 i = i + 1
-        return result if global_flag else ""
+        return result if flag else ""
 
-
-    def get_dict(self, t: str) -> dict:
+    def get_dict(self, s: str, t: str) -> dict:
         """
-        将字符串转为字典\n
-        :param t: 字符串
-        :return:  字典
+        获取字典\n
+        :param s: 第一个字符串
+        :param t: 第二个字符串
+        :return: 一个字典
         """
+        cnt_s = Counter(s)
+        cnt_t = Counter(t)
         result = dict()
-        for letter in set(list(t)):
-            result[letter] = t.count(letter)
+        for letter in cnt_t.keys():
+            result[letter] = cnt_t[letter] - cnt_s[letter]
         return result
+
+    def is_ok(self, letters: dict) -> bool:
+        """
+        是否满足条件\n
+        :param letters: 需求
+        :return: 是否满足
+        """
+        for k, v in letters.items():
+            if v > 0:
+                return False
+        return True
 
 
 if __name__ == '__main__':
     s = "ADOBECODEBANC"
     t = "ABC"
-    # s = "a"
-    # t = "a"
-    # s = "a"
-    # t = "aa"
-    # s = "a"
-    # t = "b"
+    s = "a"
+    t = "a"
+    s = "a"
+    t = "aa"
+    s = "a"
+    t = "b"
+    s = "bba"
+    t = "ab"
+    s = "acbbaca"
+    t = "aba"
+    s = "ab"
+    t = "a"
+    s = "abc"
+    t = "b"
+    s = "abc"
+    t = "ab"
+    s = "bbaac"
+    t = "aba"
     print(Solution().minWindow(s, t))
